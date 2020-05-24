434ee0e44450dc8fbca94bb810d8a2d1ff1fd491
Harsh Gupta <harshgupta951114@gmail.com>
2020-05-24 16:00:19 +0530

    Updates

---
+++ precache-manifest.db76e986cfcd6d3e016b5d335bbd4acc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d182c922dbf0e74e373bfd21729f745b",
+    "url": "/react-form-assignment/index.html"
+  },
+  {
+    "revision": "bbc7899becc421d1666e",
+    "url": "/react-form-assignment/static/css/main.c3f9bd7a.chunk.css"
+  },
+  {
+    "revision": "f19c3386efc3ba6c5b12",
+    "url": "/react-form-assignment/static/js/2.8fd6058b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-form-assignment/static/js/2.8fd6058b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc7899becc421d1666e",
+    "url": "/react-form-assignment/static/js/main.d1b61190.chunk.js"
+  },
+  {
+    "revision": "957b56b2e0f40079bb03",
+    "url": "/react-form-assignment/static/js/runtime-main.d891deb2.js"
+  }
+]);
